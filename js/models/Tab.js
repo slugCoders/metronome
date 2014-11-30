3858409fdddd2a25be888ca473cf429182ac43ac
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-11-30 20:43:02 +0100

    Modify Tab.js using backbone.js library

--- js/models/Tab.js
+++ js/models/Tab.js
@@ -1,37 +1,15 @@
+
 //This class manage tab function actived by user
-var Tab = (function () {
-//Constructor
-var constructor = function(bps,nst){
-	//private
-	var beatPerSecond = bps;
-	var nSecondsTabs = nst;
-	var active = false;
-
-	//Getters and Setters
-	this.getBeatsPerSecond = function(){
-		return beatPerSecond;
-	};
-
-	this.setBeatsPerSecond = function(bps){
-		beatPerSecond = bps;
-	};
-
-	this.isActive = function(){
-		return active;
-	};
-
-	this.setActive = function(){
-		active = !active;
+var Tab = Backbone.Model.extend({
+	beatPerSecond:100,
+	nSecondsTabs:[2.5,2]
+	actve:false,
+	beatsAvarage: function(){
+		len = this.nSecondsTabs.lenght;
+		sum = 0;
+		for(i = 0, len, sum; i < len;i++){
+			sum += this.nSecondsTabs[i];
+	 	}
+	 	return sum/len;
 	}
-};
-
-//public methods
-this.beatsAvarage = function(){
-	for(i = 0, len = this.nSecondsTabs.lenght, sum ; i < len;i++){
-		sum += this.nSecondsTabs[i];
-	}
-	return sum/len;
-};
-
-return Tab;
-})();
+})
