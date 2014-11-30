4b04d69ad603a7cd8c557f6a3f77f9f18febd6aa
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-11-30 21:07:16 +0100

    Add defaults on Tab.js

--- js/models/Tab.js
+++ js/models/Tab.js
@@ -1,9 +1,14 @@
 
 //This class manage tab function actived by user
+//Define if it's is necessary save beats per second and nSecondsTabs
+// If the change it on time we don't need saved it.
+
 var Tab = Backbone.Model.extend({
-	beatPerSecond:2,
-	nSecondsTabs:[2.5,2],
-	actve:false,
+	defaults: {
+		beatPerSecond: 2,
+		nSecondsTabs:[2.5,2],
+		actve:false
+	},
 	beatsAvarage: function(){
 		len = this.nSecondsTabs.lenght;
 		sum = 0;
