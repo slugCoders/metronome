63d147c8e2f8b64b5eb85cbf873abf2a04740c51
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2015-01-20 20:30:55 +0100

    Add playSound method to Manager.js

--- js/models/Manager.js
+++ js/models/Manager.js
@@ -34,4 +34,14 @@ var Manager = Backbone.Model.extend({function(){
 		request.send();
 	}
 
+	playSound: function(buffer, time) {
+    	var source = context.createBufferSource();
+    	source.buffer = buffer;
+    	source.connect(context.destination);
+    	if (!source.start)
+      		source.start = source.noteOn;
+    	source.start(time);
+  }
+
+
 }); 
