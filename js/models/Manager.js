9bff78f21e1f08e61fbb099a7d4eee7a64cc454c
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2015-01-21 20:55:57 +0100

    Modify Manager.js

--- js/models/Manager.js
+++ js/models/Manager.js
@@ -1,47 +1,48 @@
+$(function(){
+	var Manager = Backbone.Model.extend({function(){
 
-var Manager = Backbone.Model.extend({function(){
+		defaults: function(){
+			return {
+				tempo: null,
+				rythm: null,
+				volume: null,
+				beat: null,
+				sound: null
+			};
+		}
 
-	defaults: function(){
-		return {
-			tempo: null,
-			rythm: null,
-			volume: null,
-			beat: null,
-			sound: null
-		};
-	}
+		start: function(){
+			//TODO
+		}
+		stop: function(){
+			//TODO
+		}
 
-	start: function(){
-		//TODO
-	}
-	stop: function(){
-		//TODO
-	}
+		loadSound:function (url) {
+			var request = new XMLHttpRequest();
+			request.open('GET', url, true);
+			request.responseType = 'arraybuffer';
+			
+			request.onload = function() {
+	    	//request.response is encoded... so decode it now
+	    		context.decodeAudioData(request.response, function(buffer) {
+	    	    sound = buffer;
+	    		}, function(err) {
+	      		throw new Error(err);
+	    		});
+	  		}
+			request.send();
+		}
 
-	loadSound:function (url) {
-		var request = new XMLHttpRequest();
-		request.open('GET', url, true);
-		request.responseType = 'arraybuffer';
-		
-		request.onload = function() {
-    	//request.response is encoded... so decode it now
-    		context.decodeAudioData(request.response, function(buffer) {
-    	    sound = buffer;
-    		}, function(err) {
-      		throw new Error(err);
-    		});
-  		}
-		request.send();
-	}
+		playSound: function(buffer, time) {
+	    	var source = context.createBufferSource();
+	    	source.buffer = buffer;
+	    	source.connect(context.destination);
+	    	if (!source.start)
+	      		source.start = source.noteOn;
+			source.start(time);
+		}
 
-	playSound: function(buffer, time) {
-    	var source = context.createBufferSource();
-    	source.buffer = buffer;
-    	source.connect(context.destination);
-    	if (!source.start)
-      		source.start = source.noteOn;
-    	source.start(time);
-  }
-
-
+		return Manager;
 }); 
+});
