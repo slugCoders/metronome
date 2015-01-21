7be32efba1ff3c12efb10c4a5a3fc0ba030b77e4
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2015-01-21 20:38:12 +0100

    Modify index.php

--- index.php
+++ index.php
@@ -22,18 +22,23 @@ and open the template in the editor.
             <h1>Metronome</h1>
             <hr />
             <div class="page">
-                
-                <div>
-                    <div>
+                <form>
+                    <label>bpm/tempo:</label>
+                    <input type="number" name="quantity" min="40" max="200" value="120" />
+                     <input type="button" value="Play" />
+                     <input type="button" value="Stop" />
+                </form>
+                <!--<div>
+                    <div> 
                         <canvas data-processing-sources="assets/pde/pendulum.pde"></canvas>
-                        <!--<canvas id="number1" data-processing-sources="pde/number.pde"></canvas>-->
+                        <canvas id="number1" data-processing-sources="pde/number.pde"></canvas>
                     </div>
                     <div class="mixerDesk">
                         <ul class="mixerTracks">
 
                         </ul>
-                    </div> <!-- /mixerDesk -->
-                </div>
+                    </div>  /mixerDesk 
+                </div>-->
                 
                 
                 
@@ -44,7 +49,8 @@ and open the template in the editor.
         
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/processing.js/1.4.8/processing.min.js"></script>
-
+        <script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js"></script>
         
     </body>
 </html>
