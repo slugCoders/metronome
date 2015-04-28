eefddd7e41b0bb334abd2ebfc54e6251ee2c1a11
merry <marc.ribera.llimos@gmail.com>
2015-04-29 00:52:20 +0200

    new view

--- js/app/views/view.js
+++ js/app/views/view.js
@@ -6,6 +6,9 @@ define(function(require){
     	Backbone = require("backbone"),
     	Manager = require('app/models/manager');
   		
+	
+    	var i=0;
+    		
 	var View = Backbone.View.extend({
 		el:$(".container"),
 		/* init events, when user click element with 
@@ -14,14 +17,33 @@ define(function(require){
 		*/
 		
 		events: {
-			"click .play": "play"
-			/*"change #tempo": "play",
-			"click #stop": "stop"*/
+			"click .play": "play",
+			"click .stop": "stop",
+			"click .up" : "changeTempo",
+			"click .down" : "changeTempo"
 		},
+
 		play: function(){
 			console.log("PLAY");
-			this.model.startPlayingRithm();
+			this.model.playMetronome();
+		},
+		stop: function(){
+			console.log("STOP");
+			this.model.stopMetronome();
+		},
+		changeTempo: function(e){
+			console.log($(e.currentTarget).attr('value'));
+			i = parseInt($(e.currentTarget).attr('value'));
+			this.model.modTempo(i);
 		}
 	});
 	return new View({model:Manager});
 });
+
+// Trigger two functions in one Event.
+/*
+"click .play": function(e) {
+        	this.play(e);
+        	this.up1(e);
+    		},
+*/
